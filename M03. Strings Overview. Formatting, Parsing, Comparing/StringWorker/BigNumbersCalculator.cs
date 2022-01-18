63ea1e7fb294b826e5b490e944c27f2337314e97
Pavel Makarikhin <makarikhin.p.a@gmail.com>
2022-01-18 14:51:53 +0000

    M03 dev

---
+++ M03. Strings Overview. Formatting, Parsing, Comparing/StringWorker/BigNumbersCalculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using Ardalis.GuardClauses;
+
+namespace StringWorker
+{
+    internal static class BigNumbersCalculator
+    {
+        static bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Суммирует неотрицательные целые числа, записанные в строках strA и strB.
+        /// </summary>
+        public static string Sum(string strA, string strB)
+        {
+            Guard.Against.InvalidInput(strA, nameof(strA), x => IsDigitsOnly(x));
+            Guard.Against.InvalidInput(strB, nameof(strB), x => IsDigitsOnly(x));
+
+            var result = new StringBuilder();
+            string biggerTerm = strA.Length > strB.Length ? strA : strB;
+            string smallerTerm = strA.Length <= strB.Length ? strA : strB;
+            int digitNum;
+            int extraAdd = 0;
+
+            for (int i = 0; i < biggerTerm.Length; i++)
+            {
+                int digitBigger = int.Parse(biggerTerm[biggerTerm.Length - 1 - i].ToString());
+
+                if (i < smallerTerm.Length)
+                {
+                    int digitSmaller = int.Parse(smallerTerm[smallerTerm.Length - 1 - i].ToString());
+
+                    digitNum = digitSmaller + digitBigger + extraAdd;
+                    extraAdd = digitNum / 10;
+                    result.Insert(0, digitNum % 10);
+                }
+                else
+                {
+                    result.Insert(0, digitBigger + extraAdd);
+                    extraAdd = 0;
+                }
+            }
+
+            if (extraAdd > 0)
+            {
+                result.Insert(0, extraAdd);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ M03. Strings Overview. Formatting, Parsing, Comparing/StringWorker/WordLengthCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ardalis.GuardClauses;
+
+namespace StringWorker
+{
+    static internal class WordLengthCounter
+    {
+        /// <summary>
+        /// Считает среднюю длину слова во входящей строке, не учитываю знаки пунктуации.
+        /// </summary>
+        static public double AverageWordLength(string str)
+        {
+            Guard.Against.NullOrEmpty(str, "");
+
+            var words = str.Split(' ');
+            var avg = 0;
+
+            foreach (var word in words)
+            {
+                foreach (var sym in word)
+                {
+                    if (!Char.IsPunctuation(sym))
+                    {
+                        avg++;
+                    }
+                }
+            }
+
+            return avg / words.Length;
+        }
+    }
+}
