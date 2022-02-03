7dace4cc07b84b15f3eda642422a0bd98b7a8151
Pavel Makarikhin <makarikhin.p.a@gmail.com>
2022-02-03 17:17:18 +0000

    M05 dev

---
+++ M05. Exception Handling. Logging. NLog/LoggingConsoleApp/Program.cs
@@ -0,0 +1,84 @@
+﻿using NLog;
+using NLog.Config;
+using StringConverter;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using NLog.Extensions.Logging;
+
+namespace LoggingConsoleApp
+{
+    internal class Program
+    {
+        private static IServiceProvider BuildDi(LoggingConfiguration config)
+        {
+            return new ServiceCollection()
+                .AddTransient<IntegerParser>()
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    loggingBuilder.AddNLog(config);
+                })
+                .BuildServiceProvider();
+        }
+
+        static void InvokeParseInteger(IntegerParser parser, string stringNum)
+        {
+            try
+            {
+                Console.WriteLine(string.Format("{0} as string", stringNum));
+                Console.WriteLine(parser.ParseInteger(stringNum));
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Null string!");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Wrong format of string!");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Null or empty input!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Integer overflow!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unhandled exception!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                LogManager.LoadConfiguration("nlog.config");
+                var serviceProvider = BuildDi(LogManager.Configuration);
+
+                using (serviceProvider as IDisposable)
+                {
+                    IntegerParser parser = serviceProvider.GetRequiredService<IntegerParser>();
+
+                    InvokeParseInteger(parser, "12345");
+                    InvokeParseInteger(parser, "asjdhgyhjk");
+                    InvokeParseInteger(parser, "1235hjk");
+                    InvokeParseInteger(parser, null);
+                    InvokeParseInteger(parser, "1111111111111111111111111111111111111111111111111111111111111111111");
+                    InvokeParseInteger(parser, "-12369252");
+                    InvokeParseInteger(parser, "11-11-1111-11");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ M05. Exception Handling. Logging. NLog/StringConverter/IntegerParser.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace StringConverter
+{
+    /// <summary>
+    /// Парсер целых чисел, представленных в виде строки.
+    /// </summary>
+    public class IntegerParser
+    {
+        private static ILogger<IntegerParser> _logger;
+
+        /// <summary>
+        /// Конструктор IntegerParser.
+        /// </summary>
+        public IntegerParser(ILogger<IntegerParser> log)
+        {
+            _logger = log;
+        }
+
+        /// <summary>
+        /// Парсинг числа типа integer записанного в строке.
+        /// </summary>
+        public int ParseInteger(string num)
+        {
+            _logger.Log(LogLevel.Information, string.Format("Input: {0}", num));
+
+            if (String.IsNullOrWhiteSpace(num))
+            {
+                _logger.Log(LogLevel.Error, string.Format("Null or empty string. \n {0}", Environment.StackTrace));
+                throw new ArgumentException("Null or empty input!");
+            }
+
+            var result = 0;
+            var sign = 1;
+            var offset = 0;
+
+            try
+            {
+                if (num.IndexOf("-") == 0)
+                {
+                    sign = -1;
+                    offset = 1;
+                    _logger.Log(LogLevel.Information, string.Format("Minus from string \"{0}\" parsed", num));
+                }
+
+                for (var i = offset; i < num.Length; i++)
+                {
+                    if (Char.IsDigit(num[i]))
+                    {
+                        result = checked(result * 10 + (num[i] - '0'));
+                        _logger.Log(LogLevel.Information, string.Format("Digit \"{0}\" from string \"{1}\" parsed", num[i], num));
+                    }
+                    else
+                    {
+                        _logger.Log(LogLevel.Error, string.Format("String \"{0}\" contains incorrect symbol \"{1}\"!\n {2}", num, num[i], Environment.StackTrace));
+                        throw new ArgumentOutOfRangeException(num, num[i], "Invalid symbol in string!");
+                    }
+                }
+            }
+            catch (OverflowException ex)
+            {
+                _logger.Log(LogLevel.Error, $"{ex.Message} \n {ex.StackTrace}");
+                throw;
+            }
+
+            return result * sign;
+        }
+    }
+}
